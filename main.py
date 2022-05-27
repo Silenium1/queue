6fac38e71297d11f809a241050a48b42eea9aa31
Silenium1 <102929509+Silenium1@users.noreply.github.com>
2022-05-27 09:36:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-n = int(input())
-m = int(input())
+n = int(input()) # часы
+m = int(input()) # сколько человек проходит
 queue = []
 cust = input()
 while cust != ".":
     for i in n:
+        if cust == "Customer":
+        queue.append("Customer")
+        queue = queue[-m:]
+    else:
+        if len(queue) == 1:
+            queue.insert(0, "VIP-customer")
+        elif len(queue) % 2 == 0:
+            queue.insert(len(queue) // 2, "VIP-customer")
+            print(queue)
+        else:
+            queue.insert(len(queue) // 2 + 1, "VIP-customer")
+            print(queue)
+    cust = input()
+print(queue)
